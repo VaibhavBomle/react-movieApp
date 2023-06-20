79fbec65da43f64f7b34229a05e93f770235ad6b
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2023-06-20 10:14:24 +0530

    Implemented Movie Card Component

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # react-movieApp
+
+Practice 1: 
+45:00:00 min to 50:57:00 min
+creating a base of application
+Getting Movie API Key
+implemented fetch call and useEffect to popuate mivie data by search
+
+Practice 2: 
+50:57:00 min to 01:06:02
+implemented MovieCard component to fetch result and make iteration to display result
--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,27 +1,62 @@
-import './App.css';
-import { useEffect } from 'react';
-import SearchIcon from './search.svg';
+import "./App.css";
+import { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 //c032e2d7
 
-
 function App() {
-  const API_URL =" http://www.omdbapi.com?apikey=c032e2d7";
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+  const API_URL = " http://www.omdbapi.com?apikey={Add API Key hear}";
 
-  
-    const searchMovie =async (title) =>{
-      const response = await fetch(`${API_URL}&s=${title}`);
-      const data = await response.json();
-      console.log("data : ",data)
-    }
- 
+  // const movie1 = {
+  //   Title: "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+  //   Year: "2007",
+  //   imdbID: "tt1132238",
+  //   Type: "movie",
+  //   Poster:
+  //     "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg",
+  // };
 
-  useEffect(() =>{
-    searchMovie("Spiderman")
-  },[])
+  const searchMovie = async (title) => {
+    console.log("title : ", title);
+
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log("data : ", data);
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie(searchTerm);
+  }, []);
   return (
-    <div className='app'>
-       <h>MovieWorld</h>
-        
+    <div className="app">
+      <h1>MovieWorld</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movie"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovie(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 }
